e98f2f121586fe04bb703981044a234851da73b5
Fabio Viola <desmovalvo@gmail.com>
2016-07-05 16:22:57 +0200

    Added segmentation

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 
 # requirements
 from termcolor import colored
+from random import randint
 import numpy as np
 import getopt
+import time
 import sys
 import cv2
 
@@ -41,7 +43,35 @@ if __name__ == "__main__":
     # initialization of outputimage
     print colored("main> ", "blue", attrs=["bold"]) + "Initializing output image"
     outimage = "out.png"
-    size = (max(r_img.shape[0], l_img.shape[0]), max(r_img.shape[1], l_img.shape[1]), 1)
+    o_width = max(r_img.shape[1], l_img.shape[1])
+    o_height = max(r_img.shape[0], l_img.shape[0])
+    size = (o_height, o_width, 1)
     o_img = np.zeros(size, dtype=np.uint8)
-    cv2.imshow("output image", o_img)
+
+    # segmentation
+    print colored("main> ", "blue", attrs=["bold"]) + "Performing segmentation on image %s" % leftimage,
+    start_time = time.clock() * 1000
+    l_s_img = cv2.pyrMeanShiftFiltering(l_img, 30, 30)
+    end_time = time.clock() * 1000
+    print "%s ms" % round(end_time - start_time, 3)
+
+    print colored("main> ", "blue", attrs=["bold"]) + "Performing segmentation on image %s" % rightimage,
+    start_time = time.clock() * 1000
+    r_s_img = cv2.pyrMeanShiftFiltering(r_img, 30, 1)
+    end_time = time.clock() * 1000
+    print "%s ms" % round(end_time - start_time, 3)
+
+    # determine pixel values for the output image
+    for y in xrange(o_height):
+        for x in xrange(o_width):
+            
+            # for the given pixel we should scan the entire
+            # line of the target image and calculate the residual
+            
+
+            # pixel value
+            o_img.itemset((y, x, 0), randint(0, 255))
+
+    # display the output image
+    cv2.imshow("output image", l_s_img)
     cv2.waitKey(0)
